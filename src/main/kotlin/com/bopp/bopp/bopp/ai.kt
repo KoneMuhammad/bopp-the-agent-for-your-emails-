af9704e6171652d8e56bdbef96d8ee95aabb0013
muhamamdkone <muhammadkone@tuta.com>
2026-03-27 12:03:27 -0400

    feat - (WIP) add http get to ai model

--- src/main/kotlin/com/bopp/bopp/bopp/ai.kt
+++ src/main/kotlin/com/bopp/bopp/bopp/ai.kt
@@ -1,5 +1,34 @@
 package com.bopp.bopp.bopp
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
 
-fun llmcall(){ }
+
+val request = HttpRequest.newBuilder()
+    .uri(URI.create("https://api.github.com"))//add deepseak
+    .GET()
+    .build()
+
+class AiModel(val response: String, val SystemPrompt: String,)
+
+
+fun llmcall(){ httpClient.send<>()}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
