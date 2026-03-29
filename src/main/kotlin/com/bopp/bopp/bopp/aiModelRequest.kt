aa17f81ff9be888ff4917141755263ab02617603
muhamamdkone <muhammadkone@tuta.com>
2026-03-29 19:11:41 -0400

    feat - send post request to llm

--- src/main/kotlin/com/bopp/bopp/bopp/aiModelRequest.kt
+++ src/main/kotlin/com/bopp/bopp/bopp/aiModelRequest.kt
@@ -1,5 +1,8 @@
 package com.bopp.bopp.bopp
 
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import tools.jackson.databind.ObjectMapper
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -26,22 +29,26 @@ val body = """
 }
 """.trimIndent()
 
-
-val httpRequest = HttpRequest.newBuilder()
-    .uri(URI.create("https://api.deepseek.com/chat/completions"))
-    .header("Content-Type", "application/json")
-    .header("Authorization", "Bearer ${YOUR_API_KEY}")
-    .POST(BodyPublishers.ofString(body))
-    .build()
-
-
-val httpClient = HttpClient.newBuilder().build()
-
-val httpResponse = HttpResponse.BodyHandlers.ofString()
+    val httpRequest = HttpRequest.newBuilder()
+        .uri(URI.create("https://api.deepseek.com/chat/completions"))
+        .header("Content-Type", "application/json")
+        .header("Authorization", "Bearer ${YOUR_API_KEY}")
+        .POST(BodyPublishers.ofString(body))
+        .build()
 
 
+    val httpClient = HttpClient.newBuilder().build()
+
+    val httpResponse = HttpResponse.BodyHandlers.ofString()
 
 
+val mapper = ObjectMapper()
+
+fun sendPostToModel(){
+    val emptyStrinl: String? = null
+    val responseBody = httpClient.send(httpRequest, httpResponse)
+    val toObject = mapper.readValue(responseBody, String()::class.java)
+}
 
 
 
