a8c048d039b9065de53dbe37f7573c829fac80be
muhamamdkone <muhammadkone@tuta.com>
2026-04-09 14:36:29 -0400

    add optional parameters to request ai body

--- src/main/kotlin/com/bopp/bopp/bopp/AiController.kt
+++ src/main/kotlin/com/bopp/bopp/bopp/AiController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import com.bopp.bopp.bopp.secret.getApiKey
+import org.springframework.web.reactive.function.client.WebClient
 import tools.jackson.databind.ObjectMapper
 import java.net.URI
 import java.net.http.HttpRequest
@@ -11,6 +12,7 @@ import java.net.http.HttpRequest.BodyPublishers
 
 @RestController
 class AiController {
+
     val systemPrompt = "you will respond to me either yes or no"
     val chatQuery = "do you like machine learning?"
 
@@ -18,25 +20,49 @@ class AiController {
 {
   "model": "deepseek-chat",
   "messages": [
-    { "role": "system", "content": ${systemPrompt} },
-    { "role": "user", "content": ${chatQuery} }
+    { "role": "system", "content": "$systemPrompt" },
+    { "role": "user", "content": "$chatQuery" }
   ],
-  "stream": false
+  "thinking": { "type": "disabled" },
+  "frequency_penalty": 0,
+  "max_tokens": 4096,
+  "presence_penalty": 0,
+  "response_format": { "type": "text" },
+  "stop": null,
+  "stream": false,
+  "stream_options": null,
+  "temperature": 1,
+  "top_p": 1,
+  "tools": null,
+  "tool_choice": "none",
+  "logprobs": false,
+  "top_logprobs": null
 }
 """.trimIndent()
+
+
 
     val mapper = ObjectMapper()
     @GetMapping("/v1/ai")
     fun getModelResponse() : ResponseEntity<String>{
-        val httpRequest = HttpRequest.newBuilder()
-            .uri(URI.create("https://api.deepseek.com/chat/completions"))
-            .header("Content-Type", "application/json")
-            .header("Authorization", "Bearer ${getApiKey()}")
-            .POST(BodyPublishers.ofString(body))
-            .build()
+        val apiKey = getApiKey()
 
-        val response = httpClient.send(httpRequest, httpResponse)
-        return ResponseEntity.ok().header("Content-Type", "application/json")
-            .body(response.body())
+        val requestBody = body
+
+        val client = WebClient.create("https://api.deepseek.com")
+
+        val responseFromLLM = client.post()
+            .uri("/chat/completions")
+            .header("Content-Type", "application/json")
+            .header("Authorization", "Bearer $apiKey")
+            .bodyValue(requestBody)
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .block()  // blocking call to get the response
+
+        // Return ResponseEntity with JSON
+        return ResponseEntity.ok()
+            .header("Content-Type", "application/json")
+            .body(responseFromLLM)
     }
 }
