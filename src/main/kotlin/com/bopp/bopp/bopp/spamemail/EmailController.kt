2fd7bee2308bc02146c721e383088fcc72893be0
muhamamdkone <muhammadkone@tuta.com>
2026-04-27 12:48:58 -0400

    add notes

--- src/main/kotlin/com/bopp/bopp/bopp/spamemail/EmailController.kt
+++ src/main/kotlin/com/bopp/bopp/bopp/spamemail/EmailController.kt
@@ -37,3 +37,41 @@ class EmailController(
 }
 
     }
+
+
+
+
+client is sending json emails to me
+im sending that to llm
+llm decididing weather or not the emails are  spam
+llm produce list of ids
+i bring list to gmail api
+gmail changes emails to spam
+,, now make it agentiC,
+key is in the domain
+
+
+what would make it agentic is if i had the llm
+constantly producing outputs,
+i send 1 prompt llm produces 1 or more outputs that i consume
+
+llm produce alot without my pRompt and i just accept it
+
+2 keys
+
+the accepting it part, and
+constant input - constant output
+
+now how to constantly input and accept the constant output
+repeat function
+or a scheduled to constantly ask the llm
+Also Scheduled to constantly accept output
+with that scheduled id have to saVe the output to, a db each users local db, and fEtch from there
+
+
+how to repeat and also return? without backend
+
+
+
+how ThatNO WAY INTERG
+        one way to do that is to
